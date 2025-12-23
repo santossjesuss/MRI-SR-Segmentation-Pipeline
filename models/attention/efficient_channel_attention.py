7929138032cdcf16cede578692eb62617b3c2dfe
Jesus Santos <bsantossjesus@gmail.com>
2025-12-23 20:12:26 +0100

    fix: ECA non-instance method made instance method

--- models/attention/efficient_channel_attention.py
+++ models/attention/efficient_channel_attention.py
@@ -21,6 +21,6 @@ class ECA_Block(nn.Module):
         scaled_y = y.expand_as(x)
         return x * scaled_y
     
-    def calculate_adaptive_kernel_size(channels, gamma=2, b=1):
+    def calculate_adaptive_kernel_size(self, channels, gamma=2, b=1):
         kernel_size = int(abs(log2(channels) / gamma + b / gamma))
         return kernel_size if kernel_size % 2 == 1 else kernel_size + 1
