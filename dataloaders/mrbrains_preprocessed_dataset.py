78f1a61689f94dbc5afc750895df3a79ed106024
Jesus Santos <bsantossjesus@gmail.com>
2026-02-03 20:39:43 +0100

    refactor: MRBrainS for preprocessed dataset

--- dataloaders/mrbrains_preprocessed_dataset.py
+++ dataloaders/mrbrains_preprocessed_dataset.py
@@ -4,10 +4,11 @@ import torch
 from torch.utils.data import Dataset
 
 class MRBrainSPreprocessedDataset(Dataset):
-    def __init__(self):
-        self.dataset_path = '/data/MRBrainS/preprocessed_2d'
+    def __init__(self, dataset_path=None, transform=None):
+        self.dataset_path = self._get_dataset_path(dataset_path)
         self.images = self._load_data(self.dataset_path, isMask=False)
         self.masks = self._load_data(self.dataset_path, isMask=True)
+        self.transform = transform
 
     def __len__(self):
         return len(self.images)
@@ -17,9 +18,12 @@ class MRBrainSPreprocessedDataset(Dataset):
         mask = np.load(self.masks[idx])
 
         image = self._normalize(image)
+        image, mask = self._apply_transform(image, mask)
+        
+        image = np.transpose(image, (2, 0, 1))  # (H, W, C) -> (C, H, W)
 
-        image = torch.from_numpy(image).unsqueeze(0)    # (H, W) -> (1, H, W)
-        mask = torch.from_numpy(mask).long()            # (H, W)
+        image = torch.from_numpy(image).float()
+        mask = torch.from_numpy(mask).long()    # (H, W, 1)
 
         return image, mask
 
@@ -31,5 +35,21 @@ class MRBrainSPreprocessedDataset(Dataset):
             images_path = os.path.join(dataset_path, 'images')
             return sorted(os.listdir(images_path))
         
-    def _normalize(self):
-        pass
+    def _normalize(self, image):
+        for c in range(image.shape[2]):
+            mean = image[:, :, c].mean()
+            std = image[:, :, c].std()
+            image[:, :, c] = (image[:, :, c] - mean) / std
+        return image
+
+    def _apply_transform(self, image, mask):
+        if self.transform:
+            augmented = self.transform(image=image, mask=mask)
+            image = augmented['image']
+            mask = augmented['mask']        
+        return image, mask
+
+    def _get_dataset_path(self, dataset_path):
+        base_path = os.path.dirname(__file__)
+        default_path = os.path.join(base_path, '..', 'data', 'MRBrainS', 'preprocessed_2d')
+        return dataset_path if dataset_path is not None else default_path
