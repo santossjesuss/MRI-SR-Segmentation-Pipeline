36588dd070cb5b8a58202bd62f3e54ac0a71ecbb
Jesus Santos <bsantossjesus@gmail.com>
2026-03-14 00:31:42 +0100

    fix: Unhandled concatenation of model path

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,22 +1,21 @@
 class Experiment:
-    def __init__(self, name, pipeline, config, training_dataset, validation_dataset, test_dataset):
-        self.name = name
+    def __init__(self, name, pipeline, config, training_dataset, validation_dataset, test_dataset, data_resolution):
+        self.name = f'{name}.pth'
         self.pipeline = pipeline
         self.config = config
         self.training_dataset = training_dataset
         self.validation_dataset = validation_dataset
         self.test_dataset = test_dataset
+        self.data_resolution = data_resolution
 
     def run(self):
         print(f"Starting experiment: {self.name}")
 
-        self.name = f'{self.name}.pth'
-
-        pipeline = self.pipeline(config=self.config)
-        return pipeline.run(self.training_dataset, self.validation_dataset)
+        pipeline = self.pipeline(config=self.config, saving_path=self.name)
+        return pipeline.run(self.training_dataset, self.validation_dataset, self.data_resolution)
 
     def test(self):
         print(f"Testing experiment: {self.name}")
         
-        pipeline = self.pipeline(config=self.config)
-        return pipeline.test(self.test_dataset)
+        pipeline = self.pipeline(config=self.config, saving_path=self.name)
+        return pipeline.test(self.test_dataset, self.data_resolution)
