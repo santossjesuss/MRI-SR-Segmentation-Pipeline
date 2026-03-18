7137440a780c58dacf1a206c9e209fa32e8c5d76
Jesus Santos <bsantossjesus@gmail.com>
2026-03-18 17:40:40 +0100

    Added optional epoch logging on Logger

--- loggers/tensorboard_logger.py
+++ loggers/tensorboard_logger.py
@@ -4,15 +4,20 @@ class TensorBoardLogger():
     def __init__(self, log_dir):
         self.writer = SummaryWriter(log_dir=log_dir)
 
-    def log_metric(self, metric_name, metric_value, step, phase=""):
+    def log_metric(self, metric_name, metric_value, step=None, phase=""):
         tag = f"{phase}/{metric_name}" if phase else metric_name
-        self.writer.add_scalar(tag, metric_value, step)
-        
+        if step:
+            self.writer.add_scalar(tag, metric_value, step)
+        else:
+            self.writer.add_scalar(tag, metric_value)
 
-    def log_metrics(self, metrics_dict, step, phase=""):
+    def log_metrics(self, metrics_dict, step=None, phase=""):
         for name, value in metrics_dict.items():
             tag = f"{phase}/{name}" if phase else name
-            self.writer.add_scalar(tag, value, step)
+            if step:
+                self.writer.add_scalar(tag, value, step)
+            else:
+                self.writer.add_scalar(tag, value)
 
     def close(self):
         self.writer.close()
