c76abe0fb48a19b9b74aef293bf3d42a75da3ba3
Jesus Santos <bsantossjesus@gmail.com>
2026-03-19 02:13:08 +0100

    Refactor to reduce repetitive code in Datasets and Experiments

---
+++ datasets/base_seg_dataset.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+import os
+from torch.utils.data import Dataset
+from torchvision.io import read_image, ImageReadMode
+from transforms.base_transforms import BaseTransforms
+
+class BaseSegmentationDataset(Dataset, ABC):
+    def __init__(self, is_training, scale_factor, dataset_path=None, view='axial'):
+        super().__init__()
+        self.view = view
+        self.is_training = is_training
+
+        self.dataset_path = self._resolve_dataset_path(dataset_path=dataset_path)
+        self.images_path = self._get_data_path(is_image=True)
+        self.masks_path = self._get_data_path(is_image=False)
+        
+        self.images_names = self._get_names(data_path=self.images_path)
+        self.masks_names = self._get_names(data_path=self.masks_path)
+        
+        self.transforms = BaseTransforms(scale_factor=scale_factor)
+
+    def __len__(self):
+        return len(self.images_names)
+
+    def __getitem__(self, idx):
+        image_path = os.path.join(self.images_path, self.images_names[idx])
+        mask_path = os.path.join(self.masks_path, self.masks_names[idx])
+        
+        hr_image = read_image(image_path, mode=ImageReadMode.GRAY)
+        lr_image = self.transforms.downsample_image(hr_image)
+        hr_mask = read_image(mask_path, mode=ImageReadMode.GRAY)
+        lr_mask = self.transforms.downsample_mask(hr_mask)
+
+        hr_image = self.transforms.normalize_image(hr_image)
+        lr_image = self.transforms.normalize_image(lr_image)
+
+        hr_mask = hr_mask.squeeze(0)
+        lr_mask = lr_mask.squeeze(0)
+        hr_mask = self.transforms.normalize_binary_mask(mask=hr_mask)
+        lr_mask = self.transforms.normalize_binary_mask(mask=lr_mask)
+        
+        return hr_image, hr_mask, lr_image, lr_mask
+
+    @abstractmethod
+    def get_default_folder_name(self):
+        pass
+
+    def _resolve_dataset_path(self, dataset_path):
+        if dataset_path is not None:
+            return dataset_path
+        
+        base_path = os.path.dirname(__file__)
+        folder_name = self.get_default_folder_name()
+        
+        return os.path.join(base_path, '..', 'data', folder_name)
+    
+    def _get_data_path(self, is_image):
+        data_type = 'images' if is_image else 'labels'
+        suffix = 'Tr' if self.is_training else 'Ts'
+        folder_name = f'{data_type}{suffix}'
+
+        return os.path.join(self.dataset_path, folder_name, self.view)
+    
+    def _get_names(self, data_path):
+        return sorted(os.listdir(data_path))
--- experiments/base_experiments.py
+++ experiments/base_experiments.py
@@ -1,43 +1,107 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+from torch.utils.data import random_split
+from experiments.experiment import Experiment
+from enums.resolution_enum import Resolution
+from pipelines.super_resolution_pipeline import SuperResolutionPipeline
+from pipelines.segmentation_pipeline import SegmentationPipeline
+from pipelines.frozen_sr_frozen_seg_pipeline import FrozenSRFrozenSegPipeline
+from pipelines.frozen_sr_trainable_seg_pipeline import FrozenSRTrainableSegPipeline
+from pipelines.trainable_sr_frozen_seg_pipeline import TrainableSRFrozenSegPipeline
+from pipelines.joint_sr_seg_e2e_pipeline import JointSRSegE2EPipeline
+from pipelines.joint_sr_seg_combined_pipeline import JointSRSegCombinedPipeline
 
 class BaseExperiments(ABC):
-    def __init__(self):
+    def __init__(self, config, dataset):
         super().__init__()
+        self.config = config
 
-    @abstractmethod
-    def get_super_resolution(self):
-        pass
+        complete_train_dataset = dataset(
+            is_training=True, 
+            view=self.config.view, 
+            scale_factor=self.config.scale_factor
+        )
+        train_size, validation_size = self._get_train_validation_sizes(
+            train_dataset_size=len(complete_train_dataset), 
+            train_perc=self.config.train_perc_size
+        )
+        train_subset, validation_subset = random_split(
+            dataset=complete_train_dataset, 
+            lengths=[train_size, validation_size]
+        )
 
-    @abstractmethod
-    def get_hr_segmentation(self):
-        pass
-
-    @abstractmethod
-    def get_lr_segmentation(self):
-        pass
-
-    @abstractmethod
-    def get_frozen_sr_frozen_seg(self):
-        pass
-
-    @abstractmethod
-    def get_frozen_sr_trainable_seg(self):
-        pass
-
-    @abstractmethod
-    def get_trainable_sr_frozen_seg(self):
-        pass
-
-    @abstractmethod
-    def get_joint_sr_seg_e2e(self):
-        pass
-
-    @abstractmethod
-    def get_joint_sr_seg_combined(self):
-        pass
-
+        self.train_dataset = train_subset
+        self.validation_dataset = validation_subset
+        self.test_dataset = dataset(
+            is_training=False, 
+            view=self.config.view, 
+            scale_factor=self.config.scale_factor
+        )
+    
     def _get_train_validation_sizes(self, train_dataset_size, train_perc):
         train_size = int(train_perc * train_dataset_size)
         validation_size = train_dataset_size - train_size
 
         return train_size, validation_size
+    
+    def _create_experiment(self, name, pipeline, data_resolution=None):
+        return Experiment(
+            config=self.config,
+            name=name,
+            pipeline=pipeline,
+            training_dataset=self.train_dataset,
+            validation_dataset=self.validation_dataset,
+            test_dataset=self.test_dataset,
+            data_resolution=data_resolution
+        )
+
+    def get_super_resolution(self):
+        return self._create_experiment(
+            name=self.config.sr_name, 
+            pipeline=SuperResolutionPipeline
+        )
+
+    def get_hr_segmentation(self):
+        return self._create_experiment(
+            name=self.config.hr_seg_name, 
+            pipeline=SegmentationPipeline,
+            data_resolution=Resolution.HR
+        )
+
+    def get_lr_segmentation(self):
+        return self._create_experiment(
+            name=self.config.lr_seg_name, 
+            pipeline=SegmentationPipeline,
+            data_resolution=Resolution.LR
+        )
+
+    def get_frozen_sr_frozen_seg(self):
+        return self._create_experiment(
+            name=self.config.frozen_sr_frozen_seg, 
+            pipeline=FrozenSRFrozenSegPipeline
+        )
+
+    def get_frozen_sr_trainable_seg(self):
+        return self._create_experiment(
+            name=self.config.frozen_sr_trainable_seg, 
+            pipeline=FrozenSRTrainableSegPipeline
+        )
+
+    def get_trainable_sr_frozen_seg(self):
+        return self._create_experiment(
+            name=self.config.trainable_sr_frozen_seg, 
+            pipeline=TrainableSRFrozenSegPipeline
+        )
+
+    # This pipeline uses Segmentation Loss to learn
+    def get_joint_sr_seg_e2e(self):
+        return self._create_experiment(
+            name=self.config.joint_sr_seg_e2e, 
+            pipeline=JointSRSegE2EPipeline
+        )
+
+    # This pipeline uses both SuperRes and Segmentation Losses to learn
+    def get_joint_sr_seg_combined(self):
+        return self._create_experiment(
+            name=self.config.joint_sr_seg_combined, 
+            pipeline=JointSRSegCombinedPipeline
+        )
